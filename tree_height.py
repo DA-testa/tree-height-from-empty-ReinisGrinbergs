e60b6a846b84a2d34e75c8bb7f28a9012072bf11
ReinisGrinbergs <125680978+ReinisGrinbergs@users.noreply.github.com>
2023-03-05 21:15:45 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,51 @@
-# python3
-
 import sys
 import threading
 import numpy
 
 
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
-
-
+def compute_height(n, vecaki):
+    h = [0] * n
+    for i in range(n):
+        if not h[i]:
+            node = i
+            augst = 1
+            while vecaki[node] != -1:
+                 if h[vecaki[node]]:
+                      augst += h[vecaki[node]]
+                      break
+                 node = vecaki[node]
+                 augst += 1
+            h[i] = augst
+    return max(h)
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    inpt = input().upper()
+    vecaki = None
+    n = None
+    if inpt == "I":
+          n = int(input())
+          vecaki = list(map(int, input().split()))
+    elif inpt == "F":
+        filen = input()
+        if "a" in filen.lower():
+              print("a not allowed")
+              return
+        try:
+            with open(filen, mode="r") as f:
+                x = f.readlines()
+                n = int(x[0])
+                vecaki = list(map(int, x[1].split()))
+        except FileNotFoundError:
+           print("File does not exist")
+           return
+    else:
+        print("Incorrect input")
+        return
+    print(compute_height(n, vecaki))
+
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
